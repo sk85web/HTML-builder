2e3a2e16e987cc7d1b0c1a2be5c24ed35c6a03ff
Sergey <sk85post@gmail.com>
2024-01-22 23:10:21 +0300

    init: start last task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,3 +1,4 @@
+// 1. Import all required modules.
 const fs = require('fs/promises')
 const path = require('path')
 
@@ -5,6 +6,7 @@ const pathToProject = path.join(__dirname, 'project-dist')
 const pathToStyles = path.join(__dirname, 'styles')
 
 
+// 2. Read and save the template file in a variable.
 let htmlTemplate = ''
 const readTemplateFile = fs.readFile(path.join(__dirname, 'template.html'), 'utf-8', (err, data) => {
     if(err) {
