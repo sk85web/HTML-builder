8e8e9f63718f77e7de6c6a5882cd395f5ddd4f47
Sergey <sk85post@gmail.com>
2024-01-23 00:13:13 +0300

    feat: find tag names in the template file

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,17 +2,36 @@
 const fs = require('fs/promises')
 const path = require('path')
 
-const pathToProject = path.join(__dirname, 'project-dist')
+const pathToSite = path.join(__dirname, 'project-dist')
 const pathToStyles = path.join(__dirname, 'styles')
 
-
+async function generateHTML() {
+    fs.mkdir(pathToSite, {recursive: true})
 // 2. Read and save the template file in a variable.
-let htmlTemplate = ''
-const readTemplateFile = fs.readFile(path.join(__dirname, 'template.html'), 'utf-8', (err, data) => {
+    const readTemplateFile = await fs.readFile(path.join(__dirname, 'template.html'), 'utf-8', (err, data) => {
     if(err) {
-        console.log('Error: ' + err)
+        console.log('Error in readfile ./template.html: ' + err)
+        return 
+    }
+    })
+
+    let htmlTemplate = readTemplateFile
+
+// 3. Find all tag names in the template file.
+
+    const htmlComponents = await fs.readdir(path.join(__dirname, 'components'), (err, data) => {
+    if(err) {
+        console.log('Error in read ./components: ' + err)
         return 
     }
 })
 
-htmlTemplate = readTemplateFile
+// 4. Replace template tags with the content of component files. ---> Ошибка чтения файла, надо разбираться
+    htmlComponents.forEach(file => {
+    const readComponentsFile = fs.readFile(path.join(__dirname, 'components', file), 'utf-8', )
+       htmlTemplate += readComponentsFile
+})
+    await fs.writeFile(path.join(pathToSite, 'index.html'), htmlTemplate);
+}
+
+generateHTML() 
