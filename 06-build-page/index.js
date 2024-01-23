22e01b2a76b50829520265205499213b0c36cf98
Sergey <sk85post@gmail.com>
2024-01-23 09:51:56 +0300

    feat: add style.css in project-dist folder

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,9 +1,11 @@
 // 1. Import all required modules.
 const fs = require('fs/promises')
+const fsPromise = require('fs')
 const path = require('path')
 
 const pathToSite = path.join(__dirname, 'project-dist')
 const pathToStyles = path.join(__dirname, 'styles')
+const pathToSiteStyles = path.join(pathToSite, 'style.css')
 
 async function generateHTML() {
     fs.mkdir(pathToSite, {recursive: true})
@@ -37,3 +39,29 @@ async function generateHTML() {
 }
 
 generateHTML() 
+
+// 6. Use the script written in task **05-merge-styles** to create the `style.css` file.
+const writeStream = fsPromise.createWriteStream(path.join(__dirname, "project-dist/style.css"));
+
+fs.readdir(pathToStyles, {withFileTypes: true})
+    .then(files => {
+        files.forEach(file => {
+        if(file.isFile()) {
+            const fullFileName = file.name
+            const extFileName = path.extname(fullFileName).slice(1)
+            
+            if(extFileName === 'css') {
+                //здесь я читаю каждый файл .css
+                const readStream = fsPromise.createReadStream(path.join(pathToStyles, fullFileName))
+                //создаю событие: когда data получена, записываю по чанкам в writestream
+                readStream.on('data', (chunk) => {
+                    writeStream.write(chunk);
+                });
+            }
+        }
+    })
+    })
+    .catch(err => {
+        console.log('Error on reading: ' + err)
+    })
+    
